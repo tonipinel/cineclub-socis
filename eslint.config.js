0f79d551aca7bee9bff19d60da1df22b8a15dc57
Toni Pinel <tonipinel@gmail.com>
2026-08-29 13:45:20 +0200

    fix: desactivar react/no-unescaped-entities per al català

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,7 @@ export default [
       ...reactHooks.configs.recommended.rules,
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
+      'react/no-unescaped-entities': 'off',
       'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
     },
   },
